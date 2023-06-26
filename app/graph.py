e3948f0b696ae5d6e0a4e676cdf85d3745918790
Jesse Ragsdale <admin@jesseragsdale.com>
2023-06-26 16:18:09 -0700

    100% done with first ticket; finished with 2nd ticket but need to submit loom, 3rd ticket is work in progress

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,27 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    pass
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    graph = Chart(
+        df,
+        title=f"{y} by {x} for {target}",
+    ).mark_circle(size=100).encode(
+        x=x,
+        y=y,
+        color=target,
+        tooltip=Tooltip(df.columns.to_list())
+    ).properties(
+        width=600,
+        height=500,
+        background="white",
+        padding=20
+    ).configure(
+        axis={
+            "titlePadding": 10,
+            "labelColor": "blue"
+        }, title={
+            "fontSize": 24
+        }
+    )
+    return graph
--- app/machine.py
+++ app/machine.py
@@ -1,17 +1,30 @@
+from pandas import DataFrame
+from sklearn.ensemble import RandomForestClassifier
+import datetime
+import joblib
+
+
 class Machine:
 
-    def __init__(self, df):
-        pass
+    def __init__(self, df: DataFrame):
+        self.name = "Random Forest Classifier"
+        target = df["Rarity"]
+        features = df.drop(columns=["Rarity"])
+        self.model = RandomForestClassifier()
+        self.model.fit(features, target)
+        self.timestamp = '%s' % datetime.datetime.now()
 
-    def __call__(self, feature_basis):
-        pass
+    def __call__(self, pred_basis: DataFrame):
+        prediction, *_ = self.model.predict(pred_basis)
+        confidence, *_ = self.model.predict_proba(pred_basis)
+        return prediction, max(confidence)
 
     def save(self, filepath):
-        pass
+        joblib.dump(self.model, filepath)
 
     @staticmethod
     def open(filepath):
-        pass
+        joblib.load(filepath)
 
     def info(self):
-        pass
+        return f'Model: {self.name} <br>Initialized: {self.timestamp}'
