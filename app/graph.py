03dabd72b165a5816a2ff8f61e75bd0f76bb84fd
Jesse Ragsdale <admin@jesseragsdale.com>
2023-06-26 16:33:18 -0700

    added docstrings to graph.py

--- app/graph.py
+++ app/graph.py
@@ -3,6 +3,20 @@ from pandas import DataFrame
 
 
 def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    """
+    Creates an Altair Chart object based on the provided DataFrame and chart
+    specifications.
+
+    Parameters:
+    - df (pandas.DataFrame): The input DataFrame containing the data.
+    - x (str): The column name to be used as the x-axis in the chart.
+    - y (str): The column name to be used as the y-axis in the chart.
+    - target (str): The column name to be used for color encoding in the chart.
+
+    Returns:
+    - graph (altair.Chart): The Altair Chart object.
+
+    """
     graph = Chart(
         df,
         title=f"{y} by {x} for {target}",
@@ -20,7 +34,8 @@ def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
         axis={
             "titlePadding": 10,
             "labelColor": "blue"
-        }, title={
+        },
+        title={
             "fontSize": 24
         }
     )
